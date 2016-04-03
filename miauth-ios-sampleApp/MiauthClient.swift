9c3a0303191895ae9db378d271cb5a570cc0387f
Ilkka Pirttimaa <ilkka.pirttimaa@gmail.com>
2016-04-03 23:46:12 +0300

    Connection to miAuth app

---
+++ miauth-ios-sampleApp/MiauthClient.swift
@@ -0,0 +1,51 @@
+//
+//  miauthClient.swift
+//  miauth-ios-sampleApp
+//
+//  Created by developer on 03/04/16.
+//  Copyright © 2016 MIPsoft. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MiauthClient {
+    static let sharedInstance = MiauthClient()
+    
+    init() {
+        print("testi");
+    }
+    
+    private func miauthIsInstalled() -> Bool {
+        if let miauthURL:NSURL = NSURL(string:"miauth://test") {
+            let application:UIApplication = UIApplication.sharedApplication()
+            if (application.canOpenURL(miauthURL)) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func buttonTitle() -> String {
+        if  !miauthIsInstalled() {
+            return "Asenna miauth-sovellus";
+        }
+        else {
+            return "Tunnistaudu miauth-sovelluksella"
+        }
+    }
+    
+    func authenticate() -> Bool {
+        if let miauthURL:NSURL = NSURL(string:"miauth://authenticate?callbackurl=com.mipsoft.miauth-ios-sampleApp&app=miAuth+esimerkkiohjelma") {
+            let application:UIApplication = UIApplication.sharedApplication()
+            if (application.canOpenURL(miauthURL)) {
+                application.openURL(miauthURL);
+                return true
+            }
+        }
+        return false
+    }
+
+}
+
+
--- miauth-ios-sampleApp/ViewController.swift
+++ miauth-ios-sampleApp/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var buttonLogin:UIButton!
+    
+    var miauthClient = MiauthClient.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonLogin.setTitle(miauthClient.buttonTitle(), forState: .Normal )
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonLoginPressed(sender:UIButton!) {
+        miauthClient.authenticate()
+    }
 
 }
 
